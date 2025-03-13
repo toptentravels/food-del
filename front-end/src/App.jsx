174473249edad566fa6c750804b5aeffa55eca0b
toptentravels <toptentravels.co.uk@gmail.com>
2025-03-13 16:15:12 +0300

    Food Delivery

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./components/Navbar/navbar'"
+
+function App() {
+  return (
+    <div className='app'>
+    <navbar/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/components/Navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./navbar.css"
+
+function navbar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default navbar
