6080997dc678090b5503790adb788c37c683ca87
toptentravels <toptentravels.co.uk@gmail.com>
2025-03-26 16:47:46 +0300

    Updated Navbar styles and component

--- food-frontend/src/components/Navbar/Navbar.jsx
+++ food-frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  "./Navbar.css"
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+  const [menu,SetMenu] =  useState("home");
   return (
     <div className='navbar'>
      <img src={assets.logo} alt="" className="logo" />
      <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li onClick={()=>SetMenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>SetMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>SetMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>SetMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
      </ul>
      <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
